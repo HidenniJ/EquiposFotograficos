b131edd5839c15c6b08b94b03bfed02279ab6142
HidenniJ <131898911+HidenniJ@users.noreply.github.com>
2023-05-31 10:43:26 -0400

    Agregue el ID a cliente habia olvidado.

--- Data/Request/ClienteRequest.cs
+++ Data/Request/ClienteRequest.cs
@@ -1,7 +1,9 @@
 ﻿namespace EquiposFotograficos.Data.Request
 {
+    
     public class ClienteRequest
     {
+        public int Id { get; set; }
         public string Nombre { get; set; } = null!;
         public string Apellido { get; set; } = null!;
         public string Direccion { get; set; } = null!;
--- Data/Response/ClienteResponse.cs
+++ Data/Response/ClienteResponse.cs
@@ -2,6 +2,7 @@
 {
     public class ClienteResponse
     {
+        public int Id { get; set; }
         public string Nombre { get; set; } = null!;
         public string Apellido { get; set; }  = null!;
         public string Direccion { get; set; } = null!;
