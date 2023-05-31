3b7d83dd8d8b25f48c772382692bedb3b2ae934c
HidenniJ <131898911+HidenniJ@users.noreply.github.com>
2023-05-30 22:07:32 -0400

    IEquiposFotograficoDbContext actualizada

--- Data/Context/IEquiposFotograficoDbContext.cs
+++ Data/Context/IEquiposFotograficoDbContext.cs
@@ -6,6 +6,13 @@ namespace EquiposFotograficos.Data.Context
     public interface IEquiposFotograficoDbContext
     {
         public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<DetalleVenta> DetalleVentas { get; set; }
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<Rol> Roles { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
 
         public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
